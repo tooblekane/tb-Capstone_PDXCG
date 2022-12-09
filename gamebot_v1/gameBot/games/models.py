39d113b7a5618e3182ace68295abbf33cf40b79e
tooblekane <tooblekane@gmail.com>
2022-12-08 17:37:45 -0800

    search api added, starting to fill out template with info

--- gamebot_v1/gameBot/games/models.py
+++ gamebot_v1/gameBot/games/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from users.models import CustomUser
 
 class Game(models.Model):
-    title = models.CharField(max_length=200, blank=False)
+    name = models.CharField(max_length=200, blank=False)
+    apiId = models.CharField(max_length= 50, blank=False)
     description = models.TextField(null=True)
-    addedBy = models.ForeignKey(CustomUser, related_name="games", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
     
     
+    # addedBy = models.ForeignKey(CustomUser, related_name="games", on_delete=models.CASCADE)
 # year = models.CharField(max_length=4)
 # cover = models.ImageField(upload_to ='covers', blank=True, null=True)
--- gamebot_v1/gameBot/static/app.js
+++ gamebot_v1/gameBot/static/app.js
@@ -5,17 +5,57 @@ const app = Vue.createApp({
             csrfToken: '',
             key: '5f251131a8a34cdd8d63feb4f69c4669',
             currentUser: {},
+            game_search: '',
+            game_info: '',
+            searchInput: '',
+            gameID: 0,
+            name: '',
+            description: '',
+            genres: [],
+            storeid: 1,
+            tags: [],
+            searching: 0,
         }
     },
     methods: {
-        get_game_info(){
+        getGameInfo(gameID){
+            this.searching = 0
+            this.gameID = gameID
             axios({
-                url: `https://api.rawg.io/api/games?key=${this.key}`,
+                url: `https://api.rawg.io/api/games/${this.gameID}?key=${this.key}`,
                 method: 'GET',
             })
             .then(response => {
-                this.sales = response.data
-                // console.log(this.sales['results'][0]['id'])
+                console.log(response.data)
+                this.game_info = response.data
+                this.name = this.game_info['name']
+                this.genres = this.game_info['genres']
+                this.description = this.game_info['description']
+                this.tags = this.game_info['tags']
+                // console.log(this.gameInfo['results'][0]['id'])
+                // console.log(response.data['results'])
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+
+        gameSearch(){
+            axios({
+                // url: `https://api.rawg.io/api/games/5679?key=${this.key}`,
+                url: `https://api.rawg.io/api/games?key=${this.key}&search=${this.searchInput}`,
+                method: 'GET',
+            })
+            .then(response => {
+                this.searching = 1
+                this.game_search = response.data['results']
+                // console.log(this.game_search[2].name)
+                // console.log(this.game_search[2].id)
+                // console.log(this.search['results'][0])
+                // this.name = this.search['results'][0]['name']
+                // this.genres = this.search['results'][0]['genres']
+                // console.log(this.name)
+                // console.log(this.genres)
                 // console.log(response.data['results'])
             })
             .catch(err => {
@@ -31,13 +71,16 @@ const app = Vue.createApp({
                 this.currentUser = response.data
                 // console.log('CU', this.currentUser)
             })
-        }
+        },
+
     },
     created: function() {
-        // this.get_game_info()
         this.loadCurrentUser()
+        // this.getGameInfo()
+        // this.gameSearch()
     },
     mounted(){
+        // console.log(this.search_input.value)
         this.csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value
     }
 })
