2f34ceb4fa5e6be54519496c5c418e4aa28c76f7
tooblekane <tooblekane@gmail.com>
2022-12-13 17:35:33 -0800

    linked up the 2 apis mostly.  need to tweak still.

--- gamebot_v1/gameBot/static/app.js
+++ gamebot_v1/gameBot/static/app.js
@@ -6,17 +6,20 @@ const app = Vue.createApp({
             key: '5f251131a8a34cdd8d63feb4f69c4669',
             game_search: '',
             game_info: '',
-            searchInput: 'portal 2',
+            searchInput: '',
+            slug: '',
             genres: [],
             searching: 0,
             background_image: '',
             backgroundColor: 'blue',
             gameID: 0,
+            steamID: 0,
             currentUser: {},
             current_game: {},
             current_screenshots: {},
             current_screenshot: '',
             current_stores: {},
+            current_prices: {},
         }
     },
     methods: {
@@ -27,6 +30,7 @@ const app = Vue.createApp({
             })
             .then(response => {
                 this.searching = 1
+                // console.log(urltest)
                 // console.log(response.data['results'])
                 this.game_search = response.data['results']
             })
@@ -43,10 +47,12 @@ const app = Vue.createApp({
                 method: 'GET',
             })
             .then(response => {
-                console.log(response.data)
+                // console.log(response.data)
                 this.current_game = response.data
                 this.genres = this.current_game['genres']
                 this.background_image = this.current_game['background_image']
+                this.slug = this.current_game['slug']
+                console.log(this.slug)
                 this.getScreenshots(this.gameID)
             })
             .catch(err => {
@@ -78,11 +84,34 @@ const app = Vue.createApp({
                 method: 'GET',
             })
             .then(response => {
-                // console.log(response.data)
                 this.current_stores = response.data.results
+                // console.log(this.current_stores)
+                for (let i = 0; i < this.current_stores.length; i++) {
+                    if (this.current_stores[i].store_id === 1){
+                        // console.log(this.current_stores[i].url)
+                        // this.steamID = this.current_stores[i].url.split(/\//)[4]
+                        // console.log('Steam ID: ' + this.steamID)
+                        this.getGamePrice()
+                    }
+                }
             })
             .catch(err => {
                 console.log(err)
+            })
+        },
+
+        getGamePrice(){
+            // Wednesday---- Do this in 2 parts: Search with slug, then run a loop getting matching name and steamID (check Portal 2 to see if this works).
+            // Get dealID from that, then pull exact pricing info
+            axios({
+                // url: `https://www.cheapshark.com/api/1.0/deals?storeID=1&steamAppID=${this.steamID}`,
+                url: `https://www.cheapshark.com/api/1.0/deals?storeID=1&title=${this.slug}`,
+                method: 'GET',
+            })
+            .then(response => {
+                // console.log(url)
+                this.current_prices = response.data[0]
+                console.log(this.current_prices)
             })
         },
 
